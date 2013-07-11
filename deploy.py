986296ce60bd2cb44a379dae03b521a3aff258b5
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2013-07-11 16:59:52 +0530

    Testing FTP connection

--- deploy.py
+++ deploy.py
@@ -12,9 +12,15 @@ ftp.login(username,password)
 ftp.prot_p()
 
 data = []
-
 ftp.dir(data.append)
 
+def upload(ftp, file):
+    ext = os.path.splitext(file)[1]
+    if ext in (".txt", ".htm", ".html"):
+        ftp.storlines("STOR " + file, open(file))
+    else:
+        ftp.storbinary("STOR " + file, open(file, "rb"), 1024)
+upload(ftp, index.html)
 ftp.quit()
 
 for line in data:
