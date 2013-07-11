5f49c68a26e7bdd571738949daca54489ab78c3a
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2013-07-11 16:09:08 +0530

    Testing FTP connection

--- deploy.py
+++ deploy.py
@@ -1,4 +1,23 @@
 print "Deployment Script in Python"
 
 import os
-print os.environ['USERNAME']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+host = 'textrent.in'
+
+import ftplib
+
+ftp = ftplib.FTP(host)
+ftp.login(username,password)
+
+data = []
+
+ftp.dir(data.append)
+
+ftp.quit()
+
+for line in data:
+    print "-", line
+
+
+
