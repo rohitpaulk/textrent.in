d6639e4ae6388431c446e95700d719850c409f8b
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2013-07-12 05:39:26 +0530

    prepare.sh modified

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,11 @@
-echo "hi"
+echo "Preparing for deployment to Openshift"
+mkdir php
+mv _site/* php/*
+for i in $( ls ); do
+    if [ $i != "php" ]; then
+        echo $i
+        rm $i -r
+    fi
+done
+ls ./*
+
