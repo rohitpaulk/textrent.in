f9d86268bcf19083a0380354f87f5c551485ec2d
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2013-07-12 05:45:11 +0530

    prepare.sh modified

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,11 @@
 echo "Preparing for deployment to Openshift"
 mkdir php
-
+ls
+echo "no php should be there below"
+for i in $( ls ); do
+    if [ $i != "php" ]; then
+        echo $i
+    fi
+done
+echo "final ls"
 ls
